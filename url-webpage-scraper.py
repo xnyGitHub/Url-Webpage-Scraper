d0413348b26a2574738067f7285db250945f679e
Michael <michaelobineche99@gmail.com>
2021-10-20 15:05:12 +0100

    init

---
+++ url-webpage-scraper.py
@@ -0,0 +1,19 @@
+#Importing
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options #To make browser headless(Run in background)
+
+
+def startDriver():
+    pass
+    #Do something
+
+def printContent():
+    pass
+    #Get content
+    
+def saveToFile():
+    pass
+    #Save to file
+
+if __name__ == "__main__":
+    startDriver()
