b17ac3829074d2426dc3603ee4b9145a66209875
Michael <32583691+xnyGitHub@users.noreply.github.com>
2021-10-25 20:27:17 +0100

    Copied code from command-line scraper and made adjustments for the flask scraper

--- flask_url_scraper.py
+++ flask_url_scraper.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,url_for,redirect
 from selenium.webdriver.chrome.options import Options
-
+from selenium import webdriver
+import platform
 
 app = Flask(__name__)
 
@@ -18,8 +19,32 @@ def scrapePage():
     return render_template("scraped_page.html")
     
 def webScraper(userInput):
-    #We will scrape the content here and return it
-    pass
+    #userInput is the link (http://example.com) that the user wants to scrape
+    #Setting up the driver path
+    PATH = ""
+    if platform.system() == "Windows":
+        PATH = "./windows/chromedriver.exe"
+    elif platform.system() == "Darwin": #Mac
+        if platform.processor() == "i386" or platform.processor() == "arm":
+            PATH = "./mac-m1/chromedriver"
+        else:
+            PATH = "./mac/chromedriver"
+
+    #Setting up options
+    options = Options()
+    options.add_argument("--headless") #Allows for headless browsing 
+    driver = webdriver.Chrome(options=options, executable_path=PATH)
+
+    URL = userInput
+    
+     #Loading the website
+    try:
+        driver.get(URL)
+        content = driver.find_element_by_tag_name("body").get_attribute("innerText")
+        return content
+    except Exception as e:
+        print(e)
+        print("Please read error above")
         
 if __name__ == "__main__":
     app.run(debug=True)
