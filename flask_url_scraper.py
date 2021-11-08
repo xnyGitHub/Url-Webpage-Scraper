7aca10f904dab244f14f3385b0a5c2e07cbba492
Michael <michaelobineche99@gmail.com>
2021-11-08 09:31:52 +0000

    Refactored code to now use geckodriver - removed PATH for chromedrivers, removed webScaper function and instead used import from args-file

--- flask_url_scraper.py
+++ flask_url_scraper.py
@@ -1,57 +1,32 @@
 from flask import Flask, render_template,request,url_for,redirect,session
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-import platform
+import url_webpage_scraper
+
 
 app = Flask(__name__)
-app.config['SESSION_TYPE'] = 'memcached'
-app.config['SECRET_KEY'] = 'super secret key'
 
 @app.route('/', methods=["GET","POST"])
-def homePage():
+def homepage():
     if request.method == "POST":
-        content = webScraper(request.form['website'])
-        if content:
-            session['content'] = content
-            return redirect(url_for('scrapePage'))
+        URL = request.form['website']
+        content = url_webpage_scraper.get_content(URL)
+        
+        if content != None:
+            return render_template(
+                'scraped_page.html',content=content
+                )
         else:
-            return render_template("index.html",content = "Please make sure you enter a valid link")
+            return render_template(
+                "index.html",content = "Please make sure you enter a valid link"
+            )      
     else:
-        return render_template("index.html")
+        return render_template(
+            "index.html"
+            )
     
 @app.route('/scraped_page')
-def scrapePage():
+def scrapepage():
     #Show the content here
-    content = session.get("content",None)
-    return render_template("scraped_page.html",content=content)
-    
-def webScraper(userInput):
-    #userInput is the link (http://example.com) that the user wants to scrape
-    #Setting up the driver path
-    PATH = ""
-    if platform.system() == "Windows":
-        PATH = "./windows/chromedriver.exe"
-    elif platform.system() == "Darwin": #Mac
-        if platform.processor() == "i386" or platform.processor() == "arm":
-            PATH = "./mac-m1/chromedriver"
-        else:
-            PATH = "./mac/chromedriver"
-
-    #Setting up options
-    options = Options()
-    options.add_argument("--headless") #Allows for headless browsing 
-    driver = webdriver.Chrome(options=options, executable_path=PATH)
-
-    URL = userInput
-    
-     #Loading the website
-    try:
-        driver.get(URL)
-        content = driver.find_element_by_tag_name("body").get_attribute("innerText")
-        return content
-    except Exception as e:
-        print(e)
-        print("Please read error above")
+    return render_template("scraped_page.html")
         
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
