037a2e16bf3fd2fb6b612ac0af487f0dd4d2a364
Michael <michaelobineche99@gmail.com>
2021-11-08 09:51:09 +0000

    Renamed flask and arg app files

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,request,url_for,redirect,session
-import url_webpage_scraper
+import arg_app
 
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def homepage():
     if request.method == "POST":
         URL = request.form['website']
-        content = url_webpage_scraper.get_content(URL)
+        content = arg_app.get_content(URL)
         
         if content != None:
             return render_template(
